fdc91edd3d13659f6e6bbe6ddf94359157e1d3ce
Javohir Ismatullayev <70445308+ismatullayevs@users.noreply.github.com>
2024-12-04 23:59:35 +0500

    Add form validator

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 WIUT website for coursework
 
 Link: wiut.javohir.net
-
-Source code: github.com/ismatullayevs/newiut
--- assets/js/index.js
+++ assets/js/index.js
@@ -11,3 +11,51 @@ document.addEventListener("scroll", () => {
   }
 });
 
+const heading = document.createElement("h1");
+heading.textContent = "Westminster";
+const hero = document.querySelector("#hero .content");
+hero.appendChild(heading);
+
+document
+  .getElementById("message-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const err = document.getElementById("error-messages");
+    err.innerHTML = "";
+
+    const name = document.getElementById("name").value.trim();
+    const email = document.getElementById("email").value.trim();
+    const message = document.getElementById("message").value.trim();
+
+    let is_valid = true;
+    const errors = [];
+
+    if (name === "") {
+      errors.push("Name is required.");
+      is_valid = false;
+    }
+
+    if (email === "") {
+      errors.push("Email is required.");
+      is_valid = false;
+    } else if (!/\S+@\S+\.\S+/.test(email)) {
+      errors.push("Please enter a valid email address.");
+      is_valid = false;
+    }
+
+    if (message === "") {
+      errors.push("Message is required.");
+      is_valid = false;
+    }
+
+    if (!is_valid) {
+      errors.forEach((error) => {
+        const errorItem = document.createElement("div");
+        errorItem.textContent = error;
+        err.appendChild(errorItem);
+      });
+    } else {
+      // send data using api
+    }
+  });
